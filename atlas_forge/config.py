2bbe1aca4b6a1fb72dd980f9afbcbd33da822275
Dacksus <david.noack@fetch.ai>
2025-07-18 19:18:02 +0200

    unconfusing pydantic_settings

--- atlas_forge/config.py
+++ atlas_forge/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     database_url: str = "postgresql+psycopg://atlas:atlas@localhost:5432/atlas_forge"
 
     celery_broker_url: str = "redis://red-d1t6f2mmcj7s73b7mu80:6379"
+    celery_result_backend: str = "db+postgresql+psycopg://atlas:atlas@localhost:5432/atlas_forge"
 
     notion_token: str = "ntn_F48112944128Gtu4wJ3tGVD4RSU6wQzoBwqOVBh9tdkgDY"
 
